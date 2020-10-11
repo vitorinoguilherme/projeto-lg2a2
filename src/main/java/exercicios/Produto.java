c2df46ab5f11b0fc2eb889341d5d114e33815511
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-10-11 03:30:18 -0300

    Implementado classe Produto

---
+++ src/main/java/exercicios/Produto.java
@@ -0,0 +1,44 @@
+package exercicios;
+
+import java.util.List;
+
+public class Produto {
+    private String nomeProd;
+    private Double valorUnitario;
+    private List<Item> itens;
+
+    public Produto(String nomeProd, Double valorUnitario) {
+        this.setNomeProd(nomeProd);
+        this.setValorUnitario(valorUnitario);
+    }
+
+    public Produto() {
+
+    }
+
+    public void setNomeProd(String nomeProd) {
+        if ( nomeProd == null ) {
+            throw new IllegalArgumentException(
+                    "O nome do produto não pode ser nulo. "
+            );
+        }
+        this.nomeProd = nomeProd;
+    }
+
+    public String getNomeProd() {
+        return nomeProd;
+    }
+
+    public void setValorUnitario(Double valorUnitario) {
+        if (valorUnitario < 0.0) {
+            throw new IllegalArgumentException(
+                    "Valor unitário não pode ser menor que zero. "
+            );
+        }
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+}
