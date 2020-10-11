fc528f44805e30b318c64ad323849bb754b84829
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-10-11 03:29:39 -0300

    Implementado classe Cliente

---
+++ src/main/java/exercicios/Cliente.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class Cliente {
+    private String cpf;
+
+    public Cliente(String cpf) {
+        this.setCpf(cpf);
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf == null ) {
+            throw new IllegalArgumentException("CPF inválido.");
+        }
+        this.cpf = cpf;
+    }
+}
